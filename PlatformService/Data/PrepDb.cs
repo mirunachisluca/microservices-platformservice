b19f850011705c25ce923721cbb7b98168ba1ad0
Miruna Chisluca <68232365+miruna-chisluca@users.noreply.github.com>
2022-04-06 16:36:32 +0300

    added sql server for prod env

--- PlatformService/Data/PrepDb.cs
+++ PlatformService/Data/PrepDb.cs
@@ -1,22 +1,37 @@
-﻿using PlatformService.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
 
 namespace PlatformService.Data
 {
     public static class PrepDb
     {
-        public static void PrepPopulation(IApplicationBuilder app)
+        public static void PrepPopulation(IApplicationBuilder app, bool isProduction)
         {
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
-                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>(), isProduction);
             }
         }
 
-        private static void SeedData(AppDbContext context)
+        private static void SeedData(AppDbContext context, bool isProduction)
         {
+            if (isProduction)
+            {
+                Console.WriteLine("--> Applying migrations");
+
+                try
+                {
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"--> Error occured while migrating: {ex}");
+                }
+            }
+
             if (!context.Platforms.Any())
             {
-                Console.WriteLine("INFO: Seeding data...");
+                Console.WriteLine("--> Seeding data...");
 
                 context.Platforms.AddRange(
                     new Platform { Name = "DotNet", Publisher = "Microsoft", Cost = "Free" },
@@ -30,6 +45,7 @@ namespace PlatformService.Data
             {
                 Console.WriteLine("INFO: Data already exists");
             }
+
         }
     }
 }
